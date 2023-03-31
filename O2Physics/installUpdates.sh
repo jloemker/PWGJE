8849054812e94bbf3815f57b0e59e21dbf93b707
Johanna <j.lomker@nikhef.nl>
2023-03-31 21:49:43 +0200

    mark changed files to prepare PR for JE and Common

--- O2Physics/installUpdates.sh
+++ O2Physics/installUpdates.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-#copy file in tutorial section
+#copy file to jet tasks
 cp jetQa.cxx /home/johannalomker/alice/O2Physics/PWGJE/Tasks/jetQa.cxx
 cp CMakeLists.txt /home/johannalomker/alice/O2Physics/PWGJE/Tasks/CMakeLists.txt
 cp TrackSelectionDefaults.cxx /home/johannalomker/alice/O2Physics/Common/Core/TrackSelectionDefaults.cxx
 cp TrackSelectionDefaults.h /home/johannalomker/alice/O2Physics/Common/Core/TrackSelectionDefaults.h
-cp trackselection.cxx /home/johannalomker/alice/O2Physics/Common/TableProducer/trackselection.cxx 
+#cp trackselection.cxx /home/johannalomker/alice/O2Physics/Common/TableProducer/trackselection.cxx 
+cp jetfinder.cxx /home/johannalomker/alice/O2Physics/PWGJE/TableProducer
 
-#enter installation directory 
-cd /home/johannalomker/alice/sw/BUILD/O2Physics-latest/O2Physics/
+#enter installation directory alienv enter O2Physics/latest-master-o2 
+cd /home/johannalomker/alice/sw/BUILD/O2Physics-latest-hfTest/O2Physics
 pwd
 #rebuild whole tutorial section via ninja install
-eval $(alienv printenv O2Physics/latest ninja/latest)
+eval $(alienv printenv O2Physics/latest-hfTest-o2 ninja/latest)
 ninja install Tutorials/all 
 
