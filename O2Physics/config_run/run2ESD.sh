4b89ef0046825b4f68700b773c5491bab3dfab8f
Johanna Lömker <jloemker@yahoo.de>
2023-05-21 19:47:18 +0200

    add collision association for tracks and some binning configurables

--- O2Physics/config_run/run2ESD.sh
+++ O2Physics/config_run/run2ESD.sh
@@ -1,3 +1,4 @@
+#export OPTIONS="-b --aod-file /home/johannalomker/alice/serviceTask/PWGJE/data/AO2D.root"
 export OPTIONS="-b --configuration json://configRun2ESD.json"
 
-o2-analysis-timestamp ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-trackselection ${OPTIONS} | o2-analysis-trackextension ${OPTIONS} | o2-analysis-je-jet-finder ${OPTIONS} | o2-analysis-zdc-converter ${OPTIONS} | o2-analysis-je-jet-validation-qa ${OPTIONS}
+o2-analysis-je-jet-validation-qa ${OPTIONS} | o2-analysis-zdc-converter ${OPTIONS} | o2-analysis-timestamp ${OPTIONS} | o2-analysis-event-selection ${OPTIONS} | o2-analysis-trackselection ${OPTIONS} | o2-analysis-trackextension ${OPTIONS} | o2-analysis-je-jet-finder ${OPTIONS}  
